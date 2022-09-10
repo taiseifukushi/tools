56a5267e1697f5ed23dd64262fe157474d782b3d
husita-h <taisei.fukushi1129@gmail.com>
2022-09-11 00:43:23 +0900

    Optionで指定した引数をhashの値として取得できるように修正

--- xlsx_to_csv/script/example.rb
+++ xlsx_to_csv/script/example.rb
@@ -1,11 +1,18 @@
 require_relative "./require"
 require "optparse"
 
-option = OptionParser.new
-option.on("--t VAL") {|v| p v }
-option.parse!(ARGV)
+# https://docs.ruby-lang.org/ja/latest/class/OptionParser.html
+# option = OptionParser.new
+# option.on("-t VAL")
+# option.parse!(ARGV)
 
-target_excel_path = ARGV[0]
+params = {}
+OptionParser.new do |opt|
+  opt.on("-t Path of the Excel file to be read", "--target"){|v| params[:target] = v}
+  opt.parse!(ARGV)
+end
+
+target_excel_path = params[:target]
 xlsx = Roo::Excelx.new(target_excel_path) # => data/base/test.xlsx:1: Invalid char `\x03' in expression
 xlsx = Roo::Spreadsheet.open(target_excel_path)
 
@@ -17,6 +24,4 @@ output_file_path = "data/output/test_#{current_time}.csv"
 
 xlsx.to_csv(output_file_path) # https://www.rubydoc.info/gems/roo/2.2.0/Roo%2FBase:to_csv
 
-# ARGV[0]: 読み取り対象のエクセルファイルのパス
-# ARGV[1]: 書き出し先のCSVファイルのパス
 # bundle exec ruby script/example.rb data/base/test.xlsx data/output/test.csv
