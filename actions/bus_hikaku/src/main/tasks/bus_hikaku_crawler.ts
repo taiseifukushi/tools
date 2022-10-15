ab9afe7352fc66f1d8fc0bbf4db6b890e4977b6f
husita-h <taisei.fukushi1129@gmail.com>
2022-10-15 20:41:55 +0900

    日付と価格を取得

--- actions/bus_hikaku/src/main/tasks/bus_hikaku_crawler.ts
+++ actions/bus_hikaku/src/main/tasks/bus_hikaku_crawler.ts
@@ -1,14 +1,98 @@
 import { Browser } from "puppeteer";
 import { Page } from "puppeteer";
 import { BaseCrawler } from "./utils/base_crawler";
+const fs = require("fs");
+// import { generate } from "csv-generate";
+// const stringify = require("csv-stringify/lib/sync");
+// import { Stringify } from "csv-stringify/sync";
+const stringify = require("csv-stringify");
+// https://observablehq.com/@d3/learn-d3-data?collection=@d3/learn-d3
 
+interface Price {
+	day: number;
+	price: number;
+}
 export class BusHikakuCrawler extends BaseCrawler {
-// export default class TestCrawler extends BaseCrawler {
 	protected async crawl(_: Browser, page: Page) {
-		// const url: string = "https://www.bushikaku.net/search/tokyo_aomori/?utm_source=google&utm_medium=cpc&utm_content=Bus_aomori_tokyo&utm_campaign=509_Bus&utm_term=%E9%AB%98%E9%80%9F%E3%83%90%E3%82%B9%20%E9%9D%92%E6%A3%AE%20-%20%E6%9D%B1%E4%BA%AC&external_inflow=listing&utm_media=google&gclid=CjwKCAjwkaSaBhA4EiwALBgQaNePtoQlN2msM8W10ptZugKpPA-CiVd-EIshQ1LNNuEyQPR5-98BVhoC2YcQAvD_BwE";
-		// const url: string =	"https://www.bushikaku.net/search/tokyo_aomori/?utm_source=google&utm_medium=cpc&utm_content=Bus_aomori_tokyo&utm_campaign=509_Bus&utm_term=高速バス 青森 - 東京&external_inflow=listing&utm_media=google&gclid=CjwKCAjwkaSaBhA4EiwALBgQaNePtoQlN2msM8W10ptZugKpPA-CiVd-EIshQ1LNNuEyQPR5-98BVhoC2YcQAvD_BwE";
-		const url: string =	"https://www.bushikaku.net/search/tokyo_aomori";
-		await page.goto(url);
-		await page.screenshot({ path: "tmp/bus_hikaku.png" });
+		const url: string = "https://www.bushikaku.net/search/tokyo_aomori";
+		await Promise.all([
+			page.goto(url),
+			page.waitForNavigation({
+				waitUntil: "domcontentloaded",
+				timeout: 30000,
+			}),
+		]);
+		const priceTabaleSelector: string =
+			"#j_sys_search_calender > table > tbody > tr";
+		const extractionTable: string[][] = await page.$$eval(
+			priceTabaleSelector,
+			(rows) => {
+				return Array.from(rows, (row) => {
+					const columns = row.querySelectorAll("td");
+					return Array.from(columns, (column) => column.innerText);
+				});
+			}
+		);
+
+		console.log(extractionTable);
+		let json = this.convertTextToJson(extractionTable);
+		let csv = this.convertJsonToCsv(json);
+		console.log(csv);
+	}
+
+	private convertTextToJson(table: any): any {
+		let _data = [];
+		for (const elements of table) {
+			for (const element of elements) {
+				_data.push(this.processingData(element));
+			}
+		}
+		return JSON.stringify(_data);
+	}
+
+	private convertJsonToCsv(data: any): any {
+		Stringify(data, { header: false }, () => {
+			const path = "tmp/basu_hikaku.csv";
+			fs.writeFileSync(path);
+		});
+	}
+
+	// 三次元配列の型定義が分からない
+	// private processingExtractionTabale(table: string[][]): any {
+	// 	const processedTable = () => {
+	// 		for (const elements of table) {
+	// 			return Array.from(table, (elements) => {
+	// 				const _array = () =>{
+	// 					for (const element of elements) {
+	// 						return Array.from(elements, (element) => {
+	// 							return this.processingText(element);
+	// 						});
+	// 					}
+	// 				}
+	// 				return _array;
+	// 			});
+	// 		}
+	// 	}
+	// 	return processedTable;
+	// 	// return Array.from(table, (elements) => {
+	// 	// 	for (const elements of table) {
+	// 	// 		return Array.from(elements, (element) => {
+	// 	// 			for (const element of elements) {
+	// 	// 				return this.processingText(element);
+	// 	// 			}
+	// 	// 		});
+	// 	// 	}
+	// 	// });
+	// }
+
+	private processingData(text: string): Price {
+		let array: string[] = text.split(/\n/);
+		let day: number = parseInt(array[0]);
+		let price: number = parseInt(array[1].replace(/,|円/g, ""));
+		let _data: Price = {
+			day: day,
+			price: price,
+		};
+		return _data;
 	}
 }
