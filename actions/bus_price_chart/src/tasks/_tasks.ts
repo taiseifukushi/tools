3275c18f12e6d861954fc6cf50abb6274959ec2c
husita-h <taisei.fukushi1129@gmail.com>
2023-01-14 02:04:15 +0900

    ファイル名修正漏れ対応

--- actions/bus_price_chart/src/tasks/_tasks.ts
+++ actions/bus_price_chart/src/tasks/_tasks.ts
@@ -1,4 +1,4 @@
 import { TestCrawler } from "./test_crawler";
-import { BusHikakuCrawler } from "./bus_price_chart_crawler";
+import { BusPriceChartCrawler } from "./bus_price_chart_crawler";
 
-export const Tasks = [TestCrawler, BusHikakuCrawler];
+export const Tasks = [TestCrawler, BusPriceChartCrawler];
