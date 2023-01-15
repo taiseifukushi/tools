4d1ea9425f43c1c6a9ec39f16548346be75856ae
husita-h <taisei.fukushi1129@gmail.com>
2023-01-15 17:12:19 +0900

    xmllintでxmlをパースする

---
+++ actions/notify_news/main.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# ============
+# $ ./main.sh
+# ============
+
+set -eu
+
+# ========== VARIABLES ==========
+DIR_NAME="tmp"
+RSS_URL="https://husita-h.github.io/rss_feed_builder/rss_feed.xml"
+DATE="`date +'%Y%m%d%H%M'`"
+SAVE_RSS_FILE_NAME="news-${DATE}.rss"
+SAVE_RSS_FILE_PATH="${DIR_NAME}/${SAVE_RSS_FILE_NAME}"
+
+source ./.env
+# ===============================
+
+curl $RSS_URL -s -o $SAVE_RSS_FILE_PATH
+ls ./tmp | grep -v -E $SAVE_RSS_FILE_NAME | xargs -I FILE_NAME echo tmp/FILE_NAME | xargs rm
+echo "Save to $SAVE_RSS_FILE_PATH"
+
+# 非対話モードで取得
+links=`echo "cat rss/channel/item/link" | xmllint --shell $SAVE_RSS_FILE_PATH`
+
+function line_notify() {
+    curl -X POST $LINE_NOTIFY_API_URL \
+        -H "Authorization: Bearer $LINE_NOTIFY_ACCESS_TOKEN" \
+        -F "message=$1"
+}
+
+for link in $links
+do
+    line_notify $link
+done
+
+echo "Success to post news"
